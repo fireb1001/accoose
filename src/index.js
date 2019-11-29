c60a7bcdcdeaf5e46410dd2b72b7309a0ff3b89b
M A <fireb1001@gmail.com>
2019-11-29 15:18:25 +0200

    remove comment

--- src/index.js
+++ src/index.js
@@ -9,51 +9,6 @@ const { ApolloServer } = require("apollo-server");
 const PORT = process.env.PORT || 4000;
 
 //const typeDefs = require("./typeDefs");
-// Construct a schema, using GraphQL schema language
-
-// Provide resolver functions for your schema fields
-/*
-const resolvers = {
-  Query: {
-    //hello: (root, args, context) => "Hello هيخيوخيو!"
-    accounts: async (parent, args, { models: { accountModel } }, info) => {
-      const accounts = await accountModel.find({}).exec();
-      console.log(accounts);
-      return accounts;
-    },
-    account: async (parent, { id }, { models: { accountModel } }, info) => {
-      const account = await accountModel.findById({ _id: id }).exec();
-      return account;
-    },
-    sites: async (parent, args, { models: { siteModel } }, info) => {
-      const sites = await siteModel.find({}).exec();
-      console.log(sites);
-      return sites;
-    }
-  },
-  Mutation: {
-    createAccount: async (
-      parent,
-      { login, passhint },
-      { models: { accountModel } },
-      info
-    ) => {
-      const account = await accountModel.create({ login, passhint });
-      return account;
-    },
-    createSite: async (
-      parent,
-      { name, url, logoUrl },
-      { models: { siteModel } },
-      info
-    ) => {
-      const site = await siteModel.create({ name, url, logoUrl });
-      return site;
-    }
-  }
-};
-
-*/
 
 const server = new ApolloServer({
   typeDefs: schemas,
